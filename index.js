d9e397cf73116c1ce025c88b777b78fed6db1255
Anton Kozyr <anton.kozyr.dev@gmail.com>
2018-10-20 22:41:49 +0300

    update time for running the cron

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const rateExchangeScraperJob = require('./rate-exchange-scraper-job')
 const config = require('./config')
 
 const job = new CronJob({
-  cronTime: '00 30 20 * * *', // UTC timezone
+  cronTime: '00 30 21 * * *', // UTC timezone
   onTick () {
     console.log('Job is running...')
     rateExchangeScraperJob()
